df167a63f665720f541b87c510c97a233f863f2b
Rutwik Pande <rutwikpande2001@gmail.com>
2024-12-17 12:03:18 +0530

    First commit

---
+++ src/Slice/slice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  
+};
+
+export const atcSlice = createSlice({
+  name: "Add To Cart",
+  initialState,
+  reducers: {
+    
+  },
+});
+
+export const { } = atcSlice.actions;
+
+export default atcSlice.reducer;
